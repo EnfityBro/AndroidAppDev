911df32726d39f507a8dc8041fb6e4bbad1e5f9b
EnfityBro <enfity.bro1@gmail.com>
2026-03-12 20:42:18 +0300

    practice 3: main part

---
+++ Practice3/favoritebook/src/main/java/com/mirea/borodkinmakardmitrievich/favoritebook/SecondActivity.java
@@ -0,0 +1,39 @@
+package com.mirea.borodkinmakardmitrievich.favoritebook;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SecondActivity extends AppCompatActivity {
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        editText = (EditText) findViewById(R.id.editTextBook);
+    }
+
+    public void OnSubmitButtonClick(View view) {
+        Intent data = new Intent();
+        data.putExtra(MainActivity.USER_MESSAGE, editText.getText().toString());
+        setResult(Activity.RESULT_OK, data);
+        finish();
+    }
+}
---
+++ Practice3/sharer/src/main/java/com/mirea/borodkinmakardmitrievich/sharer/MainActivity.java
@@ -0,0 +1,62 @@
+package com.mirea.borodkinmakardmitrievich.sharer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityResultLauncher<Intent> imageActivityResultLauncher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Инициализация лаунчера
+        imageActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            if (data != null && data.getData() != null) {
+                                Log.d(MainActivity.class.getSimpleName(), "Data: " + data.getDataString());
+                            }
+                        }
+                    }
+                }
+        );
+    }
+
+    public void OnButtonClick(View view){
+        Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_TEXT, "Mirea");
+        startActivity(Intent.createChooser(intent, "Выбор за вами!"));
+    }
+
+    public void OnButton2Click(View view){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("*/*");
+        imageActivityResultLauncher.launch(intent);
+    }
+}
